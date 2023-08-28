0dbc7c46cdcb9993287ffa949f5bdd0288f750b9
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-08-28 14:46:31 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,10 +6,36 @@ radio.onReceivedValue(function (name, value) {
         y_pos = value
     }
 })
+function X_Speed () {
+    if (y_pos == 0) {
+        left_speed = 0
+    }
+    if (y_pos == 1) {
+        left_speed = 45
+    }
+    if (y_pos == 2) {
+        left_speed = 90
+    }
+    if (y_pos == 3) {
+        left_speed = 135
+    }
+    if (y_pos == 4) {
+        left_speed = 180
+    }
+}
+let left_speed = 0
 let y_pos = 0
 let x_pos = 0
 radio.setGroup(5)
+basic.showIcon(IconNames.SmallDiamond)
+basic.pause(1000)
+basic.clearScreen()
+Kitronik_VIEWTEXT32.scrollString(Kitronik_VIEWTEXT32.DisplayLine.Top, "Zaciname")
+basic.pause(1000)
+Kitronik_VIEWTEXT32.clearDisplay()
 basic.forever(function () {
     Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "X: " + x_pos)
     Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, "Y: " + y_pos)
+    servos.P0.setAngle(left_speed)
+    basic.pause(500)
 })
