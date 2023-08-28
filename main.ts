67b10323dcb43f63b938d0a490e87e313914b41d
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-08-28 14:11:11 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "x_pos") {
+        x_pos = value
+    }
+    if (name == "y_pos") {
+        y_pos = value
+    }
+})
+let y_pos = 0
+let x_pos = 0
+radio.setGroup(5)
 basic.forever(function () {
-	
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "X: " + x_pos)
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, "Y: " + y_pos)
 })
